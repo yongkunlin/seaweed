4f34f2bb951f9d185620cd10e7715b94f836224e
yongkunlin <1843515130@qq.com>
2019-08-16 14:08:12 +0800

    初始化config server

---
+++ core-config/src/main/java/com/knowledge/coreconfig/CoreConfigApplication.java
@@ -0,0 +1,18 @@
+package com.knowledge.coreconfig;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@EnableConfigServer
+@SpringBootApplication
+public class CoreConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreConfigApplication.class, args);
+    }
+
+}
